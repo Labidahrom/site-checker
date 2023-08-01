97245507487fd0fbfda1ef77b9f61d230e9680d6
Labidahrom <labidahrom@yandex.ru>
2023-08-01 15:47:37 +0600

    deploy on railway

--- site_checker/settings.py
+++ site_checker/settings.py
@@ -114,6 +114,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+broker_connection_retry_on_startup = True
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
