1b35d7119f27566d948a75e8050ac810526f0ef1
Labidahrom <labidahrom@yandex.ru>
2023-08-01 21:40:25 +0600

    fix

--- site_checker/settings.py
+++ site_checker/settings.py
@@ -9,7 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+]
 REDIS_URL = os.getenv("REDIS_URL")
 
 
